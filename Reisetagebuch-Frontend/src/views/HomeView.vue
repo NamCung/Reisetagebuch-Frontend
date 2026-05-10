9be27e6a4d9271299734a2d50cc236a20ab94269
namcu <Luis.Cung@Student.HTW-Berlin.de>
2026-05-10 18:35:26 +0200

    Homeview Beispielliste eingebunden

--- Reisetagebuch-Frontend/src/views/HomeView.vue
+++ Reisetagebuch-Frontend/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import BeispielListe from '../components/BeispielListe.vue'
 </script>
 
 <template>
   <main>
+    <BeispielListe />
     <TheWelcome />
   </main>
 </template>
